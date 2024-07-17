ef19a07167cf6eb3c1c1ed92baaeddf71ec98284
Natan <xpenatan@gmail.com>
2024-07-17 18:38:39 -0300

    update libs

--- crypto-perf/build.gradle.kts
+++ crypto-perf/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 
 dependencies {
     implementation(project(":crypto-core"))
-    implementation("org.openjdk.jmh:jmh-core:1.18")
-    kapt("org.openjdk.jmh:jmh-generator-annprocess:1.18")
-    implementation("org.bouncycastle:bcpkix-jdk15on:1.61")
-    implementation("org.apache.commons:commons-crypto:1.0.0")
+    implementation("org.openjdk.jmh:jmh-core:1.37")
+    kapt("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+    implementation("org.bouncycastle:bcpkix-jdk18on:1.78.1")
+    implementation("org.apache.commons:commons-crypto:1.2.0")
     implementation("buddy:buddy:2.0.0")
-    implementation("org.clojure:clojure:1.10.1-RC1")
+    implementation("org.clojure:clojure:1.11.3")
     implementation("at.favre.lib:hkdf:1.0.2")
-    implementation("org.conscrypt:conscrypt-openjdk-uber:2.1.0")
+    implementation("org.conscrypt:conscrypt-openjdk-uber:2.5.2")
 }
 
 val resultFile = File(projectDir, "build/jmh/")
@@ -29,6 +29,6 @@ tasks.register<JavaExec>("jmh") {
         "-rf",  "text",
         "-rff",  File(resultFile, "result.txt").path,
 //        "-e", "AesBuddy|AesCommons|AesConscrypt|AesJce",
-        "-i", "200"
+        "-i", "10"
     )
 }
